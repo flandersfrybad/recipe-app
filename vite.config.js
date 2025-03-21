f9a36a32dbf617d949fc8f9c1e86b5ecc5a05eab
flandersfrybad <152252386+flandersfrybad@users.noreply.github.com>
2025-03-21 00:30:50 -0600

    test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
 	base: '/recipe-app/', // Keep your local setting for GitHub Pages
-	plugins: [React()]
+	plugins: [react()]
   });
